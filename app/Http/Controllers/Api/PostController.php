eb94b168c79b6901bc5d4aff806396275f8f6fd7
alifian438 <alifian290126@gmail.com>
2023-01-03 11:30:15 +0700

    tambah update data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
 use Illuminate\support\Facades\validator;
+use Illuminate\support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -19,6 +20,7 @@ class PostController extends Controller
         return new PostResource(true, 'List Data Posts', $posts);
     }
 
+    // insert data
     public function store(Request $request){
 
         // validasi
@@ -30,21 +32,70 @@ class PostController extends Controller
 
         //check validasi error
         if($validator->fails()){
-            return response()->json($validator->errors(),422);
+            return response()->json($validator->errors(), 422);
         }
 
         //upload image
         $image = $request->file('image');
-        $image->storeAs('public/posts', $image->hasName());
+        $image->storeAs('public/posts', $image->hashName());
 
         //create post
-        $post = post::create([
-            'image' => $image->hasName(),
+        $post = Post::create([
+            'image' => $image->hashName(),
             'title' => $request->title,
             'content' => $request->content,
         ]);
 
         //return response
         return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
+    }
+
+    // show data
+    public function show(Post $post){
+        return new PostResource(true, 'Data Post Ditemukan!', $post);
+    }
+
+    // update data
+    public function update(Request $request, Post $post){
+
+        //validasi
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        //check jika validasi gagal
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        //check jika gambar tidak kosong
+        if ($request->hasFile('image')){
+
+            // upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            // hapus gambar
+            Storage::delete('public/posts/'.$post->image);
+            
+            // update post dengan gambar baru
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        } else {
+
+            //update tanpa gambar
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        }
+
+        // return response
+        return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
+
     }
 }
